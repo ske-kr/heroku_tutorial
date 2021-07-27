13bf80bb750da0db150a156c97269ba876b0af03
ske-kr <54828276+ske-kr@users.noreply.github.com>
2021-07-27 20:38:50 +0900

    menu page

--- app.py
+++ app.py
@@ -21,10 +21,26 @@ def index():
     # Render HTML with count variable
     return render_template("index.html", count=count)
 
+@app.route("/index")
+def home():
+    # Load current count
+    f = open("count.txt", "r")
+    count = int(f.read())
+    f.close()
+
+    # Increment the count
+    count += 1
+
+    # Overwrite the count
+    f = open("count.txt", "w")
+    f.write(str(count))
+    f.close()
+    return render_template("index.html", count=count)
+
 @app.route("/resume")
 def resume():
     
-    return render_template("before.html")    
+    return render_template("resume.html")    
 
 if __name__ == "__main__":
     app.run()
